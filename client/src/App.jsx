e11f325a30562f8d06aa9e7b530bbe56df8bc9e9
Fabrício D'Agostini <devfabriciodag@gmail.com>
2024-10-28 12:43:12 -0300

    socket

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,7 @@
 import React, { useState } from 'react';
 import io from 'socket.io-client';
 
-const socket = io('http://localhost:3001');
+const socket = io('https://socket-io-tests.onrender.com');
 
 function App() {
   const [roomCode, setRoomCode] = useState('');
